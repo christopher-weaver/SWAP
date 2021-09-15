a038933d3514243b6e7606cbc7dc00ec72b0409a
Christopher Weaver <christopher.m.weaver@outlook.com>
2021-09-14 21:45:21 -0400

    Create method of Project endpoint complete

---
+++ SWAP.Models/Models/ProjectCreate.cs
@@ -0,0 +1,33 @@
+﻿using SWAP.Data;
+using SWAP.Data.DataModels.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAP.Models.Models
+{
+    public class ProjectCreate
+    {
+        [Required]
+        public Category Category { get; set; }
+
+        [Required]
+        public Subcategory Subcategory { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+
+        [Required]
+        public SchoolDistrict SchoolDistrict { get; set; }
+
+        [Required]
+        public Consultant Consultant { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
