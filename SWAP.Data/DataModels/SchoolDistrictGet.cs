2acbb01dbe26d3d3b886f6f4859a970619db81e3
Kevin Johnson <kjohnson2827@gmail.com>
2021-09-16 19:06:23 -0400

    Fingers crossed

---
+++ SWAP.Data/DataModels/SchoolDistrictGet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAP.Data.DataModels
+{
+    public class SchoolDistrictGet
+    {
+        public string DistrictName { get; set; }
+
+        public string DistrictContact { get; set; }
+
+        public List<Project> Projects { get; set; } = new List<Project>();
+
+    }
+}
