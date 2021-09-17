2486978d89bcdfb512836b37e77a628016ce5d6c
Kevin Johnson <kjohnson2827@gmail.com>
2021-09-16 20:33:49 -0400

    Delete method and finished organization

---
+++ SWAP.Models/SchoolDistrictModels/SchoolDistrictDelete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAP.Models.SchoolDistrictPOST
+{
+   public class SchoolDistrictDelete
+    {
+        [Required]
+        public Guid Id { get; set; }
+    }
+}
